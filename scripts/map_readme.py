400bd38c9a79d4150a6ba7637283233cfcd6f23e
Rakhmanov Timur <tr.rakhmanov@gmail.com>
2023-09-16 00:52:42 +0300

    update files

---
+++ scripts/map_readme.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import re 
+
+df = pd.read_csv("https://raw.githubusercontent.com/Gevika/map_metagenome/main/data.tsv", sep="\t", decimal=".")
+
+fig, ax = plt.subplots(figsize=(15, 10), subplot_kw={'projection': ccrs.PlateCarree()})
+ax.set_title('World Map with Data Points', fontsize=16)
+
+ax.add_feature(cfeature.COASTLINE)
+ax.add_feature(cfeature.BORDERS, linestyle=':')
+ax.add_feature(cfeature.LAND, edgecolor='black')
+ax.add_feature(cfeature.LAKES, edgecolor='black')
+ax.add_feature(cfeature.RIVERS)
+ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
+
+plt.scatter(df['longitude'], df['latitude'], s=100, c='red', marker='o', label=df['geo_loc_name'])
+plt.savefig("images/map_image.png")
+
+with open('README.md', 'r') as file:
+    content = file.read()
+
+image_insert = "\n![My Map](./images/map_image.png)\n"
+
+new_content = re.sub(r'<!-- START-MAP-INSERT -->.*<!-- END-MAP-INSERT -->',
+                     f'<!-- START-MAP-INSERT -->{image_insert}<!-- END-MAP-INSERT -->',
+                     content, flags=re.DOTALL)
+
+with open('README.md', 'w') as file:
+    file.write(new_content)
