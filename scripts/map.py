b57b7e49ab1c19391d3fbb5c08a99321e1ea4f46
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-09-19 02:26:43 +0300

    Update and rename map_readme.py to map.py

--- scripts/map.py
+++ scripts/map.py
@@ -2,10 +2,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import folium
 import re 
 
 df = pd.read_csv("https://raw.githubusercontent.com/Gevika/map_metagenome/main/data/data.tsv", sep="\t", decimal=".")
 
+# map_html
+lat = list(df["latitude"])
+lon = list(df["longitude"])
+name_list = list(df["study_primary_focus"])
+archive = list(df["archive_project"])
+
+html = """
+Project name:<br>
+<a href="https://www.ncbi.nlm.nih.gov/search/all/?term=%%22%s%%22" target="_blank">%s</a><br>
+Study primary focus: %s
+"""
+
+map = folium.Map(location=[47, 2], zoom_start=3, tiles="Stamen Terrain")
+fgv = folium.FeatureGroup(name="Metagenomic samples")
+
+for lt, ln, name, archive_name in zip(lat, lon, name_list, archive):
+    iframe = folium.IFrame(html=html % (archive_name, archive_name, name), width=200, height=100)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe), fill_color='red', color='grey', fill_opacity=0.7))
+
+map.add_child(fgv)
+map.add_child(folium.LayerControl())
+
+map.save("index.html")
+
+# map_readme
 fig, ax = plt.subplots(figsize=(15, 10), subplot_kw={'projection': ccrs.PlateCarree()})
 ax.set_title('World Map with Data Points', fontsize=16)
 
@@ -20,6 +46,7 @@ plt.scatter(df['longitude'], df['latitude'], s=100, c='red', marker='o', label=d
 plt.tight_layout()
 plt.savefig("images/map_image.png", bbox_inches='tight')
 
+# update README.md
 with open('README.md', 'r') as file:
     content = file.read()
 
