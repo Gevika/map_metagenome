baee1bb93dbb1a70247b5cd4eeef272f59c4115d
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-09-20 02:12:23 +0300

    Update map.py

--- scripts/map.py
+++ scripts/map.py
@@ -8,23 +8,16 @@ import re
 
 # Load data
 df = pd.read_csv("https://raw.githubusercontent.com/Gevika/map_metagenome/main/data/data.tsv", sep="\\t", decimal=".")
-
-# Convert the 'depth' column to numeric, setting errors='coerce' to turn invalid parsing into NaN
 df['depth_numeric'] = pd.to_numeric(df['depth'], errors='coerce')
-
-# Get min and max values, excluding NaN values
 min_depth = df['depth_numeric'].min()
 max_depth = df['depth_numeric'].max()
 
 # Create map
 m = folium.Map(location=[47, 2], zoom_start=3, tiles="Stamen Terrain")
-
-# Add points to map
 for index, row in df.iterrows():
-    depth_value = str(row["depth"])
     popup_content = f'Project name:<br><a href="https://www.ncbi.nlm.nih.gov/search/all/?term={{row["archive_project"]}}" target="_blank">{{row["archive_project"]}}</a><br>Study primary focus: {{row["study_primary_focus"]}}'
     popup = folium.Popup(popup_content, max_width=300)
-    marker = folium.CircleMarker(
+    folium.CircleMarker(
         location=(row["latitude"], row["longitude"]),
         radius=6,
         popup=popup,
@@ -35,8 +28,6 @@ for index, row in df.iterrows():
 
 depth_values = [str(row["depth"]) for _, row in df.iterrows()]
 depth_values_str = json.dumps(depth_values)
-
-# Save map to HTML file
 m.save("index.html")
 
 # Additional JS to set data-depth attributes for each marker after they are created
@@ -48,32 +39,22 @@ js_code = f'''
         markers.forEach((marker, idx) => {{
             marker.setAttribute('data-depth', depths[idx]);
         }});
+        updateMap();  // Added this to immediately filter markers on page load
     }});
 </script>
 '''
 
-# Add JS to the HTML file
-with open('index.html', 'a') as file:
-    file.write(js_code)
-
-# Add controls to the HTML file using the calculated min_depth and max_depth
 controls_html = f'''
 <!-- Including noUiSlider CSS -->
 <link href="https://cdn.jsdelivr.net/npm/nouislider@14.6.4/distribute/nouislider.min.css" rel="stylesheet">
-
-<!-- Depth Slider Container -->
 <div style="margin: 20px;">
     <label for="depth-slider">Depth Range:</label>
     <div id="depth-slider" style="margin-top: 10px;"></div>
 </div>
-
-<!-- Buttons for None and Unknown -->
 <div style="margin: 20px;">
     <button id="none-button">Toggle None</button>
     <button id="unknown-button">Toggle Unknown</button>
 </div>
-
-<!-- Including noUiSlider JS -->
 <script src="https://cdn.jsdelivr.net/npm/nouislider@14.6.4/distribute/nouislider.min.js"></script>
 '''
 
@@ -85,7 +66,6 @@ controls_js = f'''
     let showUnknown = true;
 
     function updateMap() {{
-        console.log("Updating map with:", minDepth, maxDepth, showNone, showUnknown);
         const markers = document.querySelectorAll(".leaflet-marker-icon");
         markers.forEach(marker => {{
             const depthStr = marker.getAttribute("data-depth");
@@ -95,7 +75,6 @@ controls_js = f'''
                 (showNone && (depthStr === "None" || depthStr === "nan")) ||
                 (showUnknown && depthStr === "unknown")
             );
-            console.log("Is marker visible:", isVisible);
             marker.style.display = isVisible ? "block" : "none";
         }});
     }}
@@ -109,7 +88,6 @@ controls_js = f'''
         updateMap();
     }});
 
-    // Initialize noUiSlider with two handles
     const slider = document.getElementById('depth-slider');
     noUiSlider.create(slider, {{
         start: [{min_depth}, {max_depth}],
@@ -139,32 +117,26 @@ controls_js = f'''
 
 with open('index.html', 'a') as file:
     file.write(controls_html)
+    file.write(js_code)
     file.write(controls_js)
 
-# map_readme
 fig, ax = plt.subplots(figsize=(15, 10), subplot_kw={'projection': ccrs.PlateCarree()})
 ax.set_title('World Map with Data Points', fontsize=16)
-
 ax.add_feature(cfeature.COASTLINE)
 ax.add_feature(cfeature.BORDERS, linestyle=':')
 ax.add_feature(cfeature.LAND, edgecolor='black')
 ax.add_feature(cfeature.LAKES, edgecolor='black')
 ax.add_feature(cfeature.RIVERS)
 ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
-
 plt.scatter(df['longitude'], df['latitude'], s=100, c='red', marker='o', label=df['geo_loc_name'])
 plt.tight_layout()
 plt.savefig("images/map_image.png", bbox_inches='tight')
 
-# update README.md
 with open('README.md', 'r') as file:
     content = file.read()
-
 image_insert = "\n![My Map](./images/map_image.png)\n"
-
 new_content = re.sub(r'<!-- START-MAP-INSERT -->.*<!-- END-MAP-INSERT -->',
                      f'<!-- START-MAP-INSERT -->{image_insert}<!-- END-MAP-INSERT -->',
                      content, flags=re.DOTALL)
-
 with open('README.md', 'w') as file:
     file.write(new_content)
