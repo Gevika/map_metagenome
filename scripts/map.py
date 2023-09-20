e917ff8089c80f95bd0bb400ee0df873ccb9db0d
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-09-20 03:07:32 +0300

    Update map.py

--- scripts/map.py
+++ scripts/map.py
@@ -8,52 +8,81 @@ import re
 
 # Load data
 df = pd.read_csv("https://raw.githubusercontent.com/Gevika/map_metagenome/main/data/data.tsv", sep="\\t", decimal=".")
+
+# Convert the 'depth' column to numeric, setting errors='coerce' to turn invalid parsing into NaN
 df['depth_numeric'] = pd.to_numeric(df['depth'], errors='coerce')
+
+# Get min and max values, excluding NaN values
 min_depth = df['depth_numeric'].min()
 max_depth = df['depth_numeric'].max()
 
 # Create map
 m = folium.Map(location=[47, 2], zoom_start=3, tiles="Stamen Terrain")
+
+# Add points to map
 for index, row in df.iterrows():
     depth_value = str(row["depth"])
     popup_content = f'Project name:<br><a href="https://www.ncbi.nlm.nih.gov/search/all/?term={{row["archive_project"]}}" target="_blank">{{row["archive_project"]}}</a><br>Study primary focus: {{row["study_primary_focus"]}}'
     popup = folium.Popup(popup_content, max_width=300)
-    icon = folium.CustomIcon(icon_image='', html=f'<div data-depth="{depth_value}"></div>')
-    folium.Marker(
+    marker = folium.CircleMarker(
         location=(row["latitude"], row["longitude"]),
+        radius=6,
         popup=popup,
-        icon=icon
-    ).add_to(m)
+        fill_color='red',
+        color='grey',
+        fill_opacity=0.7
+    )
+    marker._template = folium.Element(f"""
+        var {{this._parent}} = L.circleMarker(
+            [{{this.lat}}, {{this.lng}}],
+            {{this.options}}
+        ).addTo({{this._parent}});
+        {{this._parent}}.bindPopup("{{this.popup}}", {{this.popupOptions}});
+        {{this._parent}}._icon.setAttribute('data-depth', '{depth_value}');
+    """)
+    marker.add_to(m)
 
 depth_values = [str(row["depth"]) for _, row in df.iterrows()]
 depth_values_str = json.dumps(depth_values)
+
+# Save map to HTML file
 m.save("index.html")
 
 # Additional JS to set data-depth attributes for each marker after they are created
 js_code = f'''
 <script>
     document.addEventListener('DOMContentLoaded', function() {{
-        const markers = document.querySelectorAll('.leaflet-marker-icon');
         const depths = {depth_values_str};
+        const markers = document.querySelectorAll(".leaflet-interactive");
         markers.forEach((marker, idx) => {{
             marker.setAttribute('data-depth', depths[idx]);
         }});
-        updateMap();  // Added this to immediately filter markers on page load
     }});
 </script>
 '''
 
+# Add JS to the HTML file
+with open('index.html', 'a') as file:
+    file.write(js_code)
+
+# Add controls to the HTML file using the calculated min_depth and max_depth
 controls_html = f'''
 <!-- Including noUiSlider CSS -->
 <link href="https://cdn.jsdelivr.net/npm/nouislider@14.6.4/distribute/nouislider.min.css" rel="stylesheet">
+
+<!-- Depth Slider Container -->
 <div style="margin: 20px;">
     <label for="depth-slider">Depth Range:</label>
     <div id="depth-slider" style="margin-top: 10px;"></div>
 </div>
+
+<!-- Buttons for None and Unknown -->
 <div style="margin: 20px;">
     <button id="none-button">Toggle None</button>
     <button id="unknown-button">Toggle Unknown</button>
 </div>
+
+<!-- Including noUiSlider JS -->
 <script src="https://cdn.jsdelivr.net/npm/nouislider@14.6.4/distribute/nouislider.min.js"></script>
 '''
 
@@ -65,7 +94,8 @@ controls_js = f'''
     let showUnknown = true;
 
     function updateMap() {{
-        const markers = document.querySelectorAll(".leaflet-marker-icon");
+        console.log("Updating map with:", minDepth, maxDepth, showNone, showUnknown);
+        const markers = document.querySelectorAll(".leaflet-interactive");
         markers.forEach(marker => {{
             const depthStr = marker.getAttribute("data-depth");
             const depth = parseFloat(depthStr);
@@ -74,6 +104,7 @@ controls_js = f'''
                 (showNone && (depthStr === "None" || depthStr === "nan")) ||
                 (showUnknown && depthStr === "unknown")
             );
+            console.log("Is marker visible:", isVisible);
             marker.style.display = isVisible ? "block" : "none";
         }});
     }}
@@ -87,6 +118,7 @@ controls_js = f'''
         updateMap();
     }});
 
+    // Initialize noUiSlider with two handles
     const slider = document.getElementById('depth-slider');
     noUiSlider.create(slider, {{
         start: [{min_depth}, {max_depth}],
@@ -116,7 +148,6 @@ controls_js = f'''
 
 with open('index.html', 'a') as file:
     file.write(controls_html)
-    file.write(js_code)
     file.write(controls_js)
 
 fig, ax = plt.subplots(figsize=(15, 10), subplot_kw={'projection': ccrs.PlateCarree()})
