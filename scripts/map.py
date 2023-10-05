320590ab777a042e6a8d802d68df4f7462599151
Evelina Garifullina <122532779+Gevika@users.noreply.github.com>
2023-10-05 16:15:00 +0300

    изменение зума

--- scripts/map.py
+++ scripts/map.py
@@ -24,7 +24,7 @@ def get_color(depth):
         return 'red'
 
 # Create a Folium map object and iterate through DataFrame rows to add markers
-m = folium.Map(location=[47, 2], zoom_start=2, tiles="http://services.arcgisonline.com/arcgis/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}", attr="© OpenStreetMap contributors")
+m = folium.Map(location=[47, 2], zoom_start=2.5, tiles="http://services.arcgisonline.com/arcgis/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}", attr="© OpenStreetMap contributors")
 for index, row in df.iterrows():
     value1 = row['archive_project']
     value2 = row['study_primary_focus']
